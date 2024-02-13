3c564bb69417d2aeecf647db6370dcc41bc1af08
Rafael Ehlert <rafaelehlert@MacBook-Pro-de-Rafael.local>
2024-02-13 15:29:52 -0300

    criado entidade Usuario

---
+++ src/main/java/curso_springboot/curso_jdev/model/Usuario.java
@@ -0,0 +1,37 @@
+package curso_springboot.curso_jdev.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Integer idade;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public Integer getIdade() {
+        return idade;
+    }
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+}
